ca987f588bd9c835703c7d1bb528cabb30ee336e
MAIN\mms1944 <bad>
2017-11-03 16:00:06 -0400

    jumpin

--- Spiders/Assets/Scripts/Movement.cs
+++ Spiders/Assets/Scripts/Movement.cs
@@ -5,14 +5,13 @@ using System.Collections.Generic;
 public class Movement : MonoBehaviour
 {
 	public Vector3 position;
-	private Vector3 velocity = new Vector3(2.0f, 0.0f, 0.0f);
+    public bool jumping;
+    private Rigidbody2D rBody;
+    private Vector3 velocity = new Vector3(2.0f, 0.0f, 0.0f);
 	public float speed = 0.0f;
-	private float maxSpeed = 7.0f;
+	private float maxSpeed = 10.0f;
 	private float slowDown = .1f;
-	private float speedIncrement = 7.0f;
-
-	private float angularSpeed = 90.0f;
-	private float angle = 0.0f;
+	private float speedIncrement = 9.0f;
 
 	private Vector3 worldSize = new Vector3(10.0f, 10.0f, 0.0f);
 
@@ -22,64 +21,90 @@ public class Movement : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-	}
+        rBody = gameObject.GetComponent<Rigidbody2D>();
+    }
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		//if (Input.GetKeyDown(KeyCode.W))
-		if(gameObject.tag == "Player")
-		{
-			//Is it moving or slowing down?
-			if(Input.GetKey(KeyCode.D))
-			{
+        if (rBody.velocity.y == 0)
+        {
+            jumping = false;
+        }
+        else
+        {
+            jumping = true;
+        }
+        Jump();
+        CheckInput();
+        
+        
+	}
+    public void Jump()
+    {
+        //jump check
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rBody.AddRelativeForce(Vector2.up * (60000) * Time.deltaTime);
+            //jumping = true;
+        }
+    }
+        private void CheckInput()
+    {
+        //if (Input.GetKeyDown(KeyCode.W))
+        if (gameObject.tag == "Player")
+        {
+            //Is it moving or slowing down?
+            if (Input.GetKey(KeyCode.D))
+            {
                 if (Input.GetKey(KeyCode.A))
                 {
                     speed = 0;
                 }
-				speed += speedIncrement * Time.deltaTime;
-			}
-			else if(Input.GetKey(KeyCode.A))
-			{
+                else
+                {
+                    speed += speedIncrement * Time.deltaTime;
+                }
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
                 if (Input.GetKey(KeyCode.D))
                 {
                     speed = 0;
                 }
-                speed -= speedIncrement * Time.deltaTime;
+                else
+                {
+                    speed -= speedIncrement * Time.deltaTime;
+                }
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
             }
             else
             {
                 speed = 0.0f;
             }
-			
-			//Check max and min speed
-			if(speed > maxSpeed)
-			{
-				speed = maxSpeed;
-			}
-            if(speed < -maxSpeed)
+
+            //Check max and min speed
+            if (speed > maxSpeed)
+            {
+                speed = maxSpeed;
+            }
+            if (speed < -maxSpeed)
             {
                 speed = -maxSpeed;
             }
-			else if(speed < 0.01f && speed > -.01f)
-			{
-				speed = 0.0f;
-			}
-			
-		}
-		
-		transform.position = position;
-		transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
-		
-		position += transform.rotation * velocity * speed * Time.deltaTime;
-	
-	}
+            else if (speed < 0.01f && speed > -.01f)
+            {
+                speed = 0.0f;
+            }
 
-	public Vector3 GetDirection()
-	{
-		return Vector3.Normalize(Quaternion.Euler(0.0f, angle, 0.0f) * velocity);
-	}
-	
-   
+        }
+
+        //transform.position = position;
+
+        position.x += velocity.x * speed * Time.deltaTime;
+
+        transform.position = new Vector3(position.x, transform.position.y, 0);
+    }
 
 }
---
+++ Spiders/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//controls players movement
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody2D rBody;
+
+    public float velocity;
+    public float walkSpeed;
+    private float currentSpeed;
+    private float forwardSpeed;
+    private float backwardsSpeed;
+    public float increment;
+    private float crouchSpeed;
+    private float crouchForwardSpeed;
+    private float crouchBackwardSpeed;
+
+    public static int animNum;
+    public bool jumping;
+
+    private Animator animator;
+
+    // Use this for initialization
+    void Start() {
+        rBody = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+        forwardSpeed = walkSpeed + increment;
+        backwardsSpeed = walkSpeed - increment - 5;
+        crouchSpeed = walkSpeed / 1.5f;
+        crouchForwardSpeed = crouchSpeed + (increment / 3f);
+        crouchBackwardSpeed = crouchSpeed - (increment / 3f) - 5;
+        currentSpeed = walkSpeed;
+        animNum = 0;
+        jumping = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (rBody.velocity.y == 0)
+        {            
+            jumping = false;
+        }
+        else
+        {
+            jumping = true;
+        }
+
+        ConstantMovement();
+
+        CheckInput();
+
+
+    }
+
+    public void ConstantMovement()
+    {
+        transform.Translate(currentSpeed * Time.deltaTime, 0, 0);
+    }
+
+    public void CheckInput()
+    {
+        Crouch();
+
+        Jump();
+
+        //walk check
+        if (Input.GetKey(KeyCode.D))
+        {
+            currentSpeed = forwardSpeed;            
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            currentSpeed = backwardsSpeed;           
+        }
+    }
+
+    public void Crouch()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {           
+            currentSpeed = crouchSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            currentSpeed = crouchForwardSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            currentSpeed = crouchBackwardSpeed;            
+        }
+        else
+        {
+            currentSpeed = walkSpeed;
+        }
+    }
+
+    public void Jump()
+    {
+        //jump check
+        if (Input.GetKeyDown(KeyCode.W) && (rBody.velocity.y < .5 && rBody.velocity.y > -.3))
+        {
+            rBody.AddRelativeForce(Vector2.up * rBody.mass * (velocity * 200) * Time.deltaTime);
+            //jumping = true;
+        }
+        else
+        {
+            currentSpeed = walkSpeed;
+        }
+    }
+
+    
+}
